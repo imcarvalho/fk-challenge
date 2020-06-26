988b7483eaa51e6271b50040ae8eb85cea64d385
Ines Carvalho <im141i@wayfair.com>
2020-06-26 17:49:27 +0200

    Adding task list

--- piano-app/src/Recorder.tsx
+++ piano-app/src/Recorder.tsx
@@ -16,6 +16,13 @@ const Button = styled.button`
     border-radius: 3px;
 `;
 
+/**
+ * @TODO:
+ * Define a song title when storing a song on stop recording
+ * Show a list of stored songs with title
+ * Enable replaying stored songs with a small play button next to the title (with correct timing of replayed keys!)
+ */
+
 function Recorder() {
     const { isRecording, setIsRecording, notes, setStartingTime } = useContext(NotesContext);
 
