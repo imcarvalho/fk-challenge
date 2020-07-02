e444747e3959335284f2d3fc4c42a711be591d37
Ines Carvalho <im141i@wayfair.com>
2020-07-02 22:12:54 +0200

    Adding cypress commands to manipulate the database state, and add some test examples

--- piano-app/cypress/integration/examples/actions.spec.js
+++ piano-app/cypress/integration/examples/actions.spec.js
@@ -2,10 +2,12 @@
 
 context("Recording Piano", () => {
     beforeEach(() => {
-        cy.visit("http://localhost:3000/");
+        cy.removeSongs();
     });
 
     it("starts recording and stopwatch runs", () => {
+        cy.visitPianoPage();
+
         cy.get("[data-cy=button-record]").should("be.visible");
         cy.get("[data-cy=button-stop]").should("not.exist");
         cy.get("[data-cy=stopwatch]").contains("00:00");
@@ -22,7 +24,9 @@ context("Recording Piano", () => {
         cy.get("[data-cy=button-stop]").click();
     });
 
-    it.only("starts recording and stopwatch runs", () => {
+    it("should show the save dialog after hitting record and playing piano keys", () => {
+        cy.visitPianoPage();
+
         cy.get("[data-cy=button-record]").click();
 
         cy.get(".ReactPiano__Key")
@@ -30,5 +34,23 @@ context("Recording Piano", () => {
             .click();
 
         cy.get("[data-cy=button-stop]").click();
+    });
+
+    it("should have a song and play it", () => {
+        cy.addSong({
+            title: "My Amazing Song",
+            keyStrokes: [
+                { midiNumber: 54, startTime: 875, endTime: 980 },
+                { midiNumber: 59, startTime: 1175, endTime: 1299 },
+                { midiNumber: 52, startTime: 1476, endTime: 1594 },
+                { midiNumber: 57, startTime: 1771, endTime: 1790 },
+                { midiNumber: 59, startTime: 1782, endTime: 1887 },
+                { midiNumber: 53, startTime: 2042, endTime: 2174 },
+                { midiNumber: 57, startTime: 2327, endTime: 2455 },
+                { midiNumber: 55, startTime: 2628, endTime: 2776 },
+            ],
+        });
+
+        cy.visitPianoPage();
     });
 });
--- piano-app/cypress/support/commands.js
+++ piano-app/cypress/support/commands.js
@@ -23,3 +23,43 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("visitPianoPage", () => {
+    cy.visit("http://localhost:3000/");
+});
+
+Cypress.Commands.add("removeSongs", () => {
+    return cy.request({
+        method: "POST",
+        url: "http://localhost:4000",
+        body: {
+            operationName: "RemoveSong",
+            variables: {},
+            query: "mutation RemoveSong { removeSongs }",
+        },
+    });
+});
+
+Cypress.Commands.add("addSong", song => {
+    return cy.request({
+        method: "POST",
+        url: "http://localhost:4000",
+        body: {
+            operationName: "AddSong",
+            variables: song,
+            query: `mutation AddSong($title: String!, $keyStrokes: [NoteInput]!) {
+              addSong(title: $title, keyStrokes: $keyStrokes) {
+                _id
+                title
+                keyStrokes {
+                  midiNumber
+                  startTime
+                  endTime
+                  __typename
+                }
+                __typename
+              }
+            }`,
+        },
+    });
+});
